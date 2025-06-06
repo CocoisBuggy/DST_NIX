5867d45640463e30e3374168dad44168f3e9443b
CocoisBuggy <g.colinharley@gmail.com>
2025-06-06 11:15:11 +0200

    Fix: use correct user and group

--- modules/config.nix
+++ modules/config.nix
@@ -15,7 +15,7 @@ in
     # initialize the main cluster config
     systemd.tmpfiles.rules = map (
       instance:
-      "f /var/lib/dstserver/${instance.cluster.NETWORK.cluster_name}/cluster.ini 0644 dstuser dstgroup - ${
+      "f /var/lib/dstserver/${instance.cluster.NETWORK.cluster_name}/cluster.ini 0750 '${cfg.userName}' '${cfg.groupName}' - ${
         lib.generators.toINI { } instance.cluster
       }"
     ) cfg.instances;
