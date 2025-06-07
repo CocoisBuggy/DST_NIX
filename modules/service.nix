b5b76741b09505bfa1a7840555b1a84232eb0d05
CocoisBuggy <g.colinharley@gmail.com>
2025-06-07 19:48:36 +0200

    ...

--- modules/service.nix
+++ modules/service.nix
@@ -60,8 +60,7 @@ let
 
         run_shared=(${installDir}/bin64/dontstarve_dedicated_server_nullrenderer_x64)
         run_shared+=(-persistent_storage_root "${cfg.dataDir}")
-        # run_shared+=(-conf_dir "${cfg.dataDir}")
-        run_shared+=(-cluster "${cluster_name}")
+        run_shared+=(-conf_dir "${cfg.dataDir}")
         run_shared+=(-monitor_parent_process $$)
 
         "''${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /' &
